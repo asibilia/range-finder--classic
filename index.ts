0e535c268278dab113585bbb9236b94c687f13a6
Alec Sibilia <sibilia.alec@gmail.com>
2023-12-16 12:12:21 -0500

    update console output

--- index.ts
+++ index.ts
@@ -1,29 +1,47 @@
 import { exec } from 'child_process'
+import chalk from 'chalk'
 
 const name = 'RangeFinder_Classic'
 const OUTPUT_DIR = `./${name}`
 
-const glob = new Bun.Glob("*");
+const glob = new Bun.Glob("*")
 for await (const filename of glob.scan("./src")) {
-    const file = Bun.file(`./src/${filename}`);
-    await Bun.write(`${OUTPUT_DIR}/${filename}`, file);
+    const file = Bun.file(`./src/${filename}`)
+    await Bun.write(`${OUTPUT_DIR}/${filename}`, file)
 }
 
-const readme = Bun.file("./README.md");
-await Bun.write(`${OUTPUT_DIR}/README.md`, readme);
+const readme = Bun.file("./README.md")
+await Bun.write(`${OUTPUT_DIR}/README.md`, readme)
 
-const license = Bun.file("./LICENSE");
-await Bun.write(`${OUTPUT_DIR}/LICENSE`, license);
+const license = Bun.file("./LICENSE")
+await Bun.write(`${OUTPUT_DIR}/LICENSE`, license)
+
+console.log(
+  chalk.white('\nZipping the project... \n'),
+)
 
 exec(`zip -r ${name}.zip ${name}`, (err, stdout, stderr) => {
   if (err) {
     // node couldn't execute the command
-    console.log(err);
-    return;
+    console.log(
+      chalk.redBright(err)
+    )
+    return
   }
 
+  console.log(
+    chalk.yellow(stdout),
+  )
+
   // the *entire* stdout and stderr (buffered)
-  console.log(`OUTPUT: ${stdout}`);
-  console.log(`ERROR: ${stderr ?? 'None'}`);
-});
+  if (stdout.length) {
+    console.log(
+      chalk.greenBright('Successfully zipped the project!'),
+    )
+  } else {
+    console.log(
+      chalk.redBright(stderr)
+    )
+  }
+})
 
